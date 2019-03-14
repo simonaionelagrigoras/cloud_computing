85279ac04dc8f0490854383ad056aaecf1f5bec8
Simona Grigoras <simona.ionela.grigoras@gmail.com>
2019-03-15 01:37:40 +0200

    CloudComputing: tema 2 - add products

--- homework_2/app/Product.php
+++ homework_2/app/Product.php
@@ -2,6 +2,124 @@
 /**
  * Created by PhpStorm.
  * User: Simona
- * Date: 14/03/2019
- * Time: 15:52
+ * Date: 28/02/2019
+ * Time: 16:24
  */
+
+class Product{
+
+    protected $dbConnection;
+    public function __construct(){
+        require_once('Connection.php');
+        $connection = new Connection();
+        $this->dbConnection = $connection->getDbConnection();
+    }
+    public function getProductsList($vendorId = null){
+        try{
+            $this->dbConnection;
+
+            $sql = "SELECT * FROM products";
+            if(!is_null($vendorId)){
+                $sql .= " WHERE vendor_id=" . $vendorId;
+            }
+            $query = $this->dbConnection->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }catch (Exception $e){
+            return ['error' => "Could not get the products: " . $e->getMessage()];
+        }
+    }
+
+    public function getProduct($productId){
+        try{
+            $this->dbConnection;
+
+            $sql = "SELECT * FROM products";
+            if(!is_null($productId)){
+                $sql .= " WHERE product_id=" . $productId;
+            }
+            $query = $this->dbConnection->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }catch (Exception $e){
+            return ['error' => "Could not get the products: " . $e->getMessage()];
+        }
+    }
+
+    public function createProduct($sku, $name, $vendorId, $description = null){
+        $sql = "SELECT * FROM vendors WHERE vendor_id=" . $vendorId;
+        $query = $this->dbConnection->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if(!count($result)){
+            return ['error' => 'Vendor with id ' . $vendorId . ' doesn\'t exist'];
+        }
+        if(strlen($sku) <3){
+            return ['error' => 'SKU must have at least 3 characters'];
+        }
+
+        if(strlen($name) <7){
+            return ['error' => 'Product_name must have at least 7 characters'];
+        }
+
+        if(is_null($vendorId)){
+            return ['error' => 'The product must be assigned to a vendor'];
+        }
+
+        try{
+            $sql = "INSERT INTO products (`sku`, `product_name`,`vendor_id`,`description`) VALUES (?,?,?,?)";
+            $stmt= $this->dbConnection->prepare($sql);
+            $stmt->execute([$sku, $name, $vendorId, $description]);
+            return ['success' => "Product created"];
+        }catch (Exception $e){
+            return ['error' => "Could not create product: " . $e->getMessage()];
+        }
+
+    }
+
+    public function updateProduct($productId, $data)
+    {
+        $update = '';
+        foreach ($data as $key => $value){
+            switch($key){
+                case 'sku':
+                    if(strlen($value) <3){
+                        return ['error' => 'SKU must have at least 3 characters'];
+                    }else{
+                        $update .= 'sku="' . $value . '",';
+                    }
+                    break;
+                case 'product_name':
+                    if(strlen($value) <7){
+                        return ['error' => 'Product_name must have at least 7 characters'];
+                    }else{
+                        $update .= 'product_name="' . $value . '",';
+                    }
+                    break;
+                case 'description':
+                    if(!is_null($value)) {
+                        $update .= 'description="' . $value . '",';
+                    }
+                    break;
+                case 'vendor_id':
+                    return ['error' => 'You cannot change the vendor of a product'];
+                    break;
+            }
+        }
+
+        try{
+            $update = rtrim($update, ',');
+            $sql = "UPDATE products SET " . $update . " WHERE product_id=" . $productId;
+            //return $sql;
+            $query= $this->dbConnection->prepare($sql);
+            $query->execute();
+            return ['success' => "Product updated"];
+        }catch (Exception $e){
+            return ['error' => "Could not update product: " . $e->getMessage()];
+        }
+
+    }
+}
---
+++ homework_2/app/products.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simona
+ * Date: 28/02/2019
+ * Time: 21:12
+ */
+
+
+$response     = [];
+$responseCode = 200;
+
+$start   = microtime(true);
+$product = null;
+$review  = null;
+function init(){
+    header("Content-Type: application/json; charset=UTF-8");
+
+    require_once('Product.php');
+    require_once('Review.php');
+
+    global $product;
+    global $review;
+    $product = new Product();
+    $review  = new Review();
+
+}
+
+function authenticate($type){
+    $headers = apache_request_headers();
+    if(!isset($headers['Authorization'])){
+        return false;
+    }
+    $authorized = false;
+    $api_key = str_replace('Bearer ', '', $headers['Authorization']);
+    $encodedKey = file_get_contents('../storage/auth_' . $type .'.txt');
+    // this could be a MYSQL query that parses an API Key table, for example
+    if($api_key == base64_decode($encodedKey)) {
+        $authorized = true;
+    } else if ($api_key == NULL) {
+
+    } else {
+        $authorized = false;
+    }
+    return $authorized;
+}
+
+function checkBodyRequest(){
+    # Get JSON as a string
+    $postDataJson = file_get_contents('php://input');
+
+    # Get as an object
+    $postData = json_decode($postDataJson, true);
+    if(is_null($postData)){
+        $response = ['error' => 'Decoding error: check json body'];
+        return $response;
+    }
+    $response = [];
+    if(!isset($postData['sku'])){
+        $response = ['error' => '%sku% is a required field'];
+        return $response;
+    }
+
+    if(!isset($postData['product_name'])){
+        $response = ['error' => '%product_name% is a required field'];
+        return $response;
+    }
+
+    if(!isset($postData['vendor_id'])){
+        $response =  ['error' => '%vendor_id% is a required field'];
+        return $response;
+    }
+
+    return $postData;
+}
+
+function checkBodyPatchRequest(){
+    # Get JSON as a string
+    $postDataJson = file_get_contents('php://input');
+
+    # Get as an object
+    $postData = json_decode($postDataJson, true);
+    if(!isset($postData['product_id'])){
+        $response = ['error' => 'Field %product_id% is required for this operation'];
+        return $response;
+    }
+    if(!isset($postData['sku']) && !isset($postData['product_name']) && !isset($postData['description'])){
+        $response = ['error' => 'You need to send at least one of the fields %sku%, %product_name% or %description% for this operation'];
+        return $response;
+    }
+
+    return $postData;
+}
+function checkBodyReviewRequest(){
+    # Get JSON as a string
+    $postDataJson = file_get_contents('php://input');
+
+    # Get as an object
+    $postData = json_decode($postDataJson, true);
+    if(!isset($postData['description'])){
+        $response = ['error' => 'Field %description% is required for this operation'];
+        return $response;
+    }
+    if(!isset($postData['rating'])){
+        $response = ['error' => 'Field %rating% is required'];
+        return $response;
+    }
+
+    return $postData;
+}
+
+function runAddRequest(){
+    global $responseCode;
+    global $product;
+    $postData = checkBodyRequest();
+
+    if(isset($postData['error'])){
+
+        $responseCode = 400;
+        return $postData;
+    }
+    $description = isset($postData['description']) ? $postData['description'] : null;
+    $response = $product->createProduct($postData['sku'], $postData['product_name'], $postData['vendor_id'], $description);
+    $responseCode = 200;
+
+    return $response;
+}
+
+function runAddReview($productId){
+    global $responseCode;
+    global $review;
+
+    $postData = checkBodyReviewRequest();
+    if(isset($postData['error'])){
+        $responseCode = 400;
+        return $postData;
+    }
+    $customerName = isset($postData['customer_name']) ? $postData['customer_name'] : null;
+    $response = $review->createReview($productId, $postData['description'], $postData['rating'], $customerName);
+    $responseCode = 201;
+
+    return $response;
+}
+
+/**
+ * Get the given variable from $_REQUEST or from the url
+ * @param string $variableName
+ * @param mixed $default
+ * @return mixed|null
+ */
+function getParam($variableName, $default = null) {
+
+    // Was the variable actually part of the request
+    if(array_key_exists($variableName, $_REQUEST))
+        return $_REQUEST[$variableName];
+
+    // Was the variable part of the url
+    $urlParts = explode('/', preg_replace('/\?.+/', '', $_SERVER['REQUEST_URI']));
+    $position = array_search($variableName, $urlParts);
+    if($position !== false && array_key_exists($position+1, $urlParts))
+        return $urlParts[$position+1];
+
+    return $default;
+}
+function getUrlParam($productId) {
+
+    $url = $_SERVER['REQUEST_URI'];
+    $url = str_replace('/cc/homework_2/app/products/' . $productId,'', $url);
+    $url = explode('/', $url);
+    $lastPart = array_pop($url);
+
+    return $lastPart;
+}
+
+function runPatchRequest(){
+    global $responseCode;
+    global $product;
+    $postData = checkBodyPatchRequest();
+    if(isset($postData['error'])){
+
+        $responseCode = 400;
+        return $postData;
+    }
+    $productId = $postData['product_id'];
+    $response = $product->updateProduct($productId, $postData);
+    $responseCode = 200;
+
+    return $response;
+}
+
+init();
+
+try{
+    switch ($_SERVER['REQUEST_METHOD']){
+        case 'POST':
+
+                $productId = getParam('products');
+                $param = getUrlParam($productId);
+                if (strlen($param) && $param != 'review') {
+                    $responseCode = 404;
+                    $response = ['error' => 'Request does not match any route'];
+                }
+                if ($productId) {
+                    if(!authenticate("customer")){
+                        $responseCode = 403;
+                        $response = ['error' => 'Consumer is not authorized to access %resources'];
+                    }else {
+                        $response = runAddReview($productId);
+                    }
+                } else {
+                    if(!authenticate("admin") || !authenticate("vendor")){
+                        $responseCode = 403;
+                        $response = ['error' => 'Consumer is not authorized to access %resources'];
+                    }else {
+                        $response = runAddRequest();
+                    }
+                }
+                //var_dump(pathinfo ("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"));
+                $responseCode = 201;
+
+            break;
+        case 'PATCH':
+            if(!authenticate("admin") && !authenticate("vendor")){
+                $responseCode = 403;
+                $response = ['error' => 'Consumer is not authorized to access %resources'];
+            }else {
+                $response = runPatchRequest();
+            }
+            break;
+        case 'GET':
+            $productId = getParam('products');
+
+            if(!empty($productId)){
+
+                $param  = getUrlParam($productId);
+                if(strlen($param) && $param != 'reviews'){
+                    $responseCode = 404;
+                    return ['error' => 'Request does not match any route'];
+                }
+                if(strlen($param)){
+                    $response = $review->getReviewsList($productId);
+                    if(!count($response)){
+                        $response = ["notice" =>  "There are no reviews for product with id " . $productId];
+                        $responseCode = 404;
+                    }
+                }else{
+                    $response = $product->getProduct($productId);
+                }
+            }else{
+                $response = $product->getProductsList();
+            }
+
+            if(isset($response['error'])){
+                $responseCode = 503;
+            }
+            break;
+        default:
+            $response = ['error' => 'Unrecognized request type'];
+    }
+
+    if(isset($response['error'])){
+        $responseCode = 400;
+    }
+}catch (Error $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}catch (ErrorException $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}catch (Exception $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}
+
+
+
+$time_elapsed_secs = microtime(true) - $start;
+http_response_code($responseCode);
+header('X-PHP-Response-Code: ' . $responseCode, true, $responseCode);
+if(!in_array($responseCode, [200,201])){
+    $response['http_status'] = $responseCode;
+    $response['time'] = $time_elapsed_secs;
+}
+echo json_encode($response);
