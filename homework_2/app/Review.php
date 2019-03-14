b163a6e4ea8fd36856d18b322f7d0b026bdf8f86
Simona Grigoras <simona.ionela.grigoras@gmail.com>
2019-03-15 01:37:49 +0200

    CloudComputing: tema 2 - add reviews

--- homework_2/app/Review.php
+++ homework_2/app/Review.php
@@ -2,6 +2,115 @@
 /**
  * Created by PhpStorm.
  * User: Simona
- * Date: 14/03/2019
- * Time: 15:53
+ * Date: 28/02/2019
+ * Time: 16:24
  */
+
+class Review{
+
+    protected $dbConnection;
+
+    public function __construct(){
+        require_once('Connection.php');
+        $connection = new Connection();
+        $this->dbConnection = $connection->getDbConnection();
+    }
+    public function getReviewsList($productId = null){
+        try{
+            $this->dbConnection;
+            $sql = "SELECT * FROM reviews ";
+            if(!is_null($productId)){
+                $sql .= " WHERE product_id=" . $productId;
+            }
+            $query = $this->dbConnection->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }catch (Exception $e){
+            return ['error' => "Could not get reviews: " . $e->getMessage()];
+        }
+    }
+
+    public function createReview($productId, $description, $rating, $customerName = null){
+
+        $sql = "SELECT * FROM products WHERE product_id=" . $productId;
+        $query = $this->dbConnection->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if(!count($result)){
+            return ['error' => 'Product with id ' . $productId . ' doesn\'t exist'];
+        }
+        if(strlen($description) <3){
+            return ['error' => 'Description must have at least 3 characters'];
+        }
+
+        if(!in_array($rating,[1,2,3,4,5])){
+            return ['error' => 'the rating must be a value between 1 and 5'];
+        }
+
+        if(is_null($customerName) || !strlen($customerName)){
+            $customerName = 'anon';
+        }
+        //var_dump($customerName);exit;
+        try{
+            $sql = "INSERT INTO reviews (`product_id`, `customer_name`, `rating`,`description`) VALUES (?,?,?,?)";
+            $stmt= $this->dbConnection->prepare($sql);
+            $stmt->execute([$productId, $customerName, $rating, $description]);
+            return ['success' => "Review created"];
+        }catch (Exception $e){
+            return ['error' => "Could not create review: " . $e->getMessage()];
+        }
+    }
+
+    public function delete($reviewId){
+        if(!strlen($reviewId)){
+            return ['error' => 'Review id is required'];
+        }
+
+        $sql = "SELECT * FROM reviews WHERE id=" . $reviewId;
+        $query = $this->dbConnection->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        if(!count($result)){
+            return ['error' => 'Review with id ' . $reviewId . ' doesn\'t exist'];
+        }
+
+        try{
+            //$pdo = $this->connect();
+            $pdo = $this->dbConnection;
+
+            $sql = "DELETE FROM `reviews` WHERE id=$reviewId";
+            $query = $pdo->prepare($sql);
+            $query->execute();
+            return ['success' => "Review Deleted"];
+        }catch (Exception $e){
+            return ['error' => "Could not delete user" . $e->getMessage()];
+        }
+
+    }
+
+    public function updateReview($companyName, $email, $cui){
+        if(strlen($companyName) <3){
+            return ['error' => 'Name must have at least 3 characters'];
+        }
+
+        if(strlen($email) <7){
+            return ['error' => 'Email must have at least 7 characters'];
+        }
+
+        if(!is_numeric($cui)){
+            return ['error' => 'CUI must be a number'];
+        }
+
+        try{
+            $sql = "INSERT INTO reviews (`company_name`, `email`, `cui`) VALUES (?,?,?)";
+            $stmt= $this->dbConnection->prepare($sql);
+            $stmt->execute([$companyName, $email, $cui]);
+            return ['success' => "Review created"];
+        }catch (Exception $e){
+            return ['error' => "Could not create review: " . $e->getMessage()];
+        }
+
+    }
+}
---
+++ homework_2/app/reviews.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simona
+ * Date: 28/02/2019
+ * Time: 21:12
+ */
+
+
+$response     = [];
+$responseCode = 200;
+
+$start   = microtime(true);
+$review  = null;
+function init(){
+    header("Content-Type: application/json; charset=UTF-8");
+    require_once('Review.php');
+
+    global $review;
+    $review  = new Review();
+
+}
+
+function authenticate($type){
+    $headers = apache_request_headers();
+    if(!isset($headers['Authorization'])){
+        return false;
+    }
+    $authorized = false;
+    $api_key = str_replace('Bearer ', '', $headers['Authorization']);
+    $encodedKey = file_get_contents('../storage/auth_' . $type .'.txt');
+    // this could be a MYSQL query that parses an API Key table, for example
+    if($api_key == base64_decode($encodedKey)) {
+        $authorized = true;
+    } else if ($api_key == NULL) {
+
+    } else {
+        $authorized = false;
+    }
+    return $authorized;
+}
+
+
+/**
+ * Get the given variable from $_REQUEST or from the url
+ * @param string $variableName
+ * @param mixed $default
+ * @return mixed|null
+ */
+function getParam($variableName, $default = null) {
+
+    // Was the variable actually part of the request
+    if(array_key_exists($variableName, $_REQUEST))
+        return $_REQUEST[$variableName];
+
+    // Was the variable part of the url
+    $urlParts = explode('/', preg_replace('/\?.+/', '', $_SERVER['REQUEST_URI']));
+    $position = array_search($variableName, $urlParts);
+    if($position !== false && array_key_exists($position+1, $urlParts))
+        return $urlParts[$position+1];
+
+    return $default;
+}
+function getUrlParam($reviewId) {
+
+    $url = $_SERVER['REQUEST_URI'];
+    $url = str_replace('/cc/homework_2/app/reviews/' . $reviewId,'', $url);
+    $url = explode('/', $url);
+    $lastPart = array_pop($url);
+
+    return $lastPart;
+}
+
+
+init();
+
+try{
+    switch ($_SERVER['REQUEST_METHOD']){
+        case 'DELETE':
+            if(!authenticate("customer")){
+                $responseCode = 403;
+                $response = ['error' => 'Consumer is not authorized to access %resources'];
+            }else {
+                $reviewId = getParam('reviews');
+
+                if ($reviewId) {
+                    $response = $review->delete($reviewId);
+                } else {
+                    $response = ['error' => '%review_id% is required'];
+                }
+                $responseCode = 200;
+            }
+            break;
+        case 'GET':
+
+            $response = $review->getReviewsList();
+            if(!count($response)){
+                $response = ["notice" =>  "There are no reviews "];
+                $responseCode = 404;
+            }
+            if(isset($response['error'])){
+                $responseCode = 503;
+            }
+            break;
+        default:
+            $response = ['error' => 'Unrecognized request type'];
+    }
+
+    if(isset($response['error'])){
+        $responseCode = 400;
+    }
+}catch (Error $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}catch (ErrorException $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}catch (Exception $e){
+    $responseCode = 500;
+    $response = ['error' => 'Internal server Error. ' . $e->getMessage()];
+}
+
+
+
+$time_elapsed_secs = microtime(true) - $start;
+http_response_code($responseCode);
+header('X-PHP-Response-Code: ' . $responseCode, true, $responseCode);
+if(!in_array($responseCode, [200,201])){
+    $response['http_status'] = $responseCode;
+    $response['time'] = $time_elapsed_secs;
+}
+echo json_encode($response);
